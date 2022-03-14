38ca7f7b87ebd94536e3b4a8e2f61d7038f7b61d
TaHasib981 <tahaisb981@gmail.com>
2022-03-14 21:26:13 +0600

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//--------distructure
 let animle = undefined
 /* {
     name : 'cow',
@@ -5,7 +6,7 @@ let animle = undefined
     quantity: 10
 } */
 let {name, ...hasib} = animle || {}
-console.log(hasib);
+// console.log(hasib);
 
 //-------------- dilstructure by function 
 let animles ={
@@ -14,7 +15,47 @@ let animles ={
     quantitys: 10
 } 
 const sum = (details) => {
-    console.log(details.names);
-    console.log(details.colors);
+ /*    console.log(details.names);
+    console.log(details.colors); */
 }
 sum(animles)
+
+//===================================================================
+
+// nested object of array
+
+const dreamGirl = [
+    {
+      dream1: {
+        name: "bbu",
+        height: "5.4",
+        family: [{ father: "rock", mother: "shila", sister: "chinki" }],
+        age: undefined,
+        contactInfo: [
+          {
+            facebook: {
+              link: "https://www.facebook.com/",
+              followers: "12545",
+              status: "single",
+              friendsList: [
+                { name: "rofik" },
+                { name: "jobbar" },
+                { name: "salam" },
+                { name: "borkot" },
+                // undefined,
+              ],
+            },
+          },
+          { instagram: "https://www.instagram.com/" },
+          { twitter: "https://twitter.com/" },
+          { github: "https://github.com/" },
+          { phone: ["01254823212", "02152457"] },
+        ],
+      },
+    },
+  ];
+const friends = dreamGirl[0].dream1.contactInfo[0].facebook.friendsList
+console.log(friends);
+for(const friend of friends){
+    console.log(friend);
+}
